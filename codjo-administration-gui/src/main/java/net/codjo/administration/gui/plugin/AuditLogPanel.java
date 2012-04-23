c6a3511dc15ad1d050c9d68bc2aab7e9a452952a
CREGO <externe.sebastien.crego@allianzgi.fr>
2012-04-23 15:06:22 +0200

    Internationalization of gui components.

--- codjo-administration-gui/src/main/java/net/codjo/administration/gui/plugin/AuditLogPanel.java
+++ codjo-administration-gui/src/main/java/net/codjo/administration/gui/plugin/AuditLogPanel.java
@@ -3,8 +3,12 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import net.codjo.i18n.gui.InternationalizableContainer;
+import net.codjo.i18n.gui.TranslationNotifier;
+import net.codjo.mad.gui.framework.GuiContext;
+import net.codjo.mad.gui.i18n.InternationalizationUtil;
 
-public class AuditLogPanel {
+public class AuditLogPanel implements InternationalizableContainer {
     private JPanel mainPanel;
     private JComboBox logs;
     private JButton readLog;
@@ -37,5 +41,16 @@ public class AuditLogPanel {
 
     public JTextArea getLog() {
         return log;
+    }
+
+
+    public void init(GuiContext guiContext) {
+        TranslationNotifier translationNotifier = InternationalizationUtil.retrieveTranslationNotifier(guiContext);
+        translationNotifier.addInternationalizableContainer(this);
+    }
+
+
+    public void addInternationalizableComponents(TranslationNotifier translationNotifier) {
+        translationNotifier.addInternationalizableComponent(readLog, "AuditLogPanel.readLogButton", null);
     }
 }
